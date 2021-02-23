2403d2449c7b6bccc757ae23ce469e77b208767b
szleo123 <leoliyilin@163.com>
2021-02-22 16:13:03 -0800

    add eval.py

---
+++ huggingface/eval.py
@@ -0,0 +1,10 @@
+from transoformers import pipeline
+
+fill_mask = pipeline(
+    "fill-mask",
+    model="esperberto",
+    tokenizer="esperberto"
+)
+
+print(fill_mask("You drink the <mask>"))
+print(fill_mask("You <mask> off"))
