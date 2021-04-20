a404e5cd8cb719165e98acb6254b9316d68653c5
szleo123 <leoliyilin@163.com>
2021-04-19 18:32:54 -0700

    1

--- own_version/trainer.py
+++ own_version/trainer.py
@@ -50,10 +50,10 @@ def training():
     train_dataset = OwnDataset(tokenizer, "../Data_non/train_no_space.en.txt")
     train_loader = DataLoader(train_dataset, batch_size=16, shuffle=False)
     n_batches = len(train_loader)
-    optim = SGD(model.parameters(), lr=1e-4, momentum=0.9, weight_decay=0.01)
+    optim = SGD(model.parameters(), lr=1e-4, momentum=0.9)
     #optim = AdamW(model.parameters(), lr=5e-5, weight_decay=0.01)
     n_epochs = 10
-    scheduler = get_linear_schedule_with_warmup(optim, 0, n_batches*n_epochs)
+    #scheduler = get_linear_schedule_with_warmup(optim, 0, n_batches*n_epochs)
     print("=== STARTING TRAINING ===")
     for epoch in range(n_epochs):
         total_loss = 0.0
