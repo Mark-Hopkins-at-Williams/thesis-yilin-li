4e7fa67640a6a4c05b16972c1f7e9cc47557f20b
szleo123 <leoliyilin@163.com>
2021-04-25 18:28:26 -0700

    1

--- own_version/prob.py
+++ own_version/prob.py
@@ -1,7 +1,7 @@
 import torch
 from tokenizers import Tokenizer
 
-model_dir = './UniLM_non_spaced'
+model_dir = './UniLM_spaced'
 
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 tokenizer = Tokenizer.from_file(model_dir + "/vocab.json")
@@ -37,9 +37,36 @@ def show_next(text):
             result[i+1] = (token, prob)
         print(result)
 
-show_next("Paris is the capital of")
+def perplexity():
+    file_path = '../Data/test2012.txt'
+    from tqdm import tqdm
+    import torch
+    with open(file_path, encoding="utf-8") as f:
+        test = f.read()
+    encodings = tokenizer.encode(test)
+    max_length = 1024
+    stride = 128
 
+    lls = []
+    for i in tqdm(range(0, len(encodings.ids), stride)):
+        begin_loc = max(i + stride - max_length, 0)
+        end_loc = min(i + stride, len(encodings.ids))
+        trg_len = end_loc - i  # may be different from stride on last loop
+        input_ids = encodings.ids[begin_loc:end_loc]
+        input_ids = torch.tensor(input_ids, dtype=torch.long).to(device)
+        target_ids = input_ids.clone().to(device)
+        target_ids[:-trg_len] = -100
 
+        with torch.no_grad():
+            outputs = model(input_ids, labels=target_ids)
+            log_likelihood = outputs[0] * trg_len
 
+        lls.append(log_likelihood)
+
+    ppl = torch.exp(torch.stack(lls).sum() / end_loc)
+    return ppl
+
+print(perplexity())
+#show_next("Paris is the capital of")
 #show_prob("the professor says that doing research is fun")
 #show_prob("the prefassor say that doing research is fun")
